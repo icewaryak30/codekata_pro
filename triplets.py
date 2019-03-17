fd2e1b84a9c864e8700a511a738c7fd61558679e
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-03-17 10:30:55 +0530

    Create triplets.py

---
+++ triplets.py
@@ -0,0 +1,9 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+y=[]
+x=[[l[i],l[j],l[k]]  for i in range(len(l)) for j in range(len(l)) for k in range(len(l)) if l[i]<l[j]<l[k] and i<j<k and i!=j!=k]
+print(x)
+for i in x:
+	if i not in y:
+		y.append(i)
+print(y)
