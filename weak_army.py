d6490c8fec34f7134e8228d337444ff30a330a31
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-03-17 11:20:57 +0530

    Create weak_army.py

---
+++ weak_army.py
@@ -0,0 +1,8 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+y=[]
+x=[[l[i],l[j],l[k]]  for i in range(len(l)) for j in range(len(l)) for k in range(len(l)) if l[i]>l[j]>l[k] and i<j<k and i!=j!=k]
+for i in x:
+	if i not in y:
+		y.append(i)
+print(len(y))
