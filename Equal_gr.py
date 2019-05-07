6915d06c195d1d343dd057cde238093422b1c07e
icewaryak30 <46440998+icewaryak30@users.noreply.github.com>
2019-05-07 13:25:55 +0530

    Update Equal_gr.py

--- Equal_gr.py
+++ Equal_gr.py
@@ -1,8 +1,23 @@
-n,k=map(int,input().split())
-l=list(map(int,input().split()))
-d=0
-for i in l:
-	if i<=(5-k):
-		d+=1
-g=d//3
-print(g)
+#ice
+n,a,b=map(int,input().split())
+m=n//2
+c=0
+if n%2==1:
+	print("NO")
+else:
+	for i in range(1,m+1):
+		for j in range(1,m+1):
+			if i*a==m:
+				print("YES")
+				c+=1
+				break
+			elif i*b==n:
+				print("YES")
+				c+=1
+				break
+			elif i*a + j*b==m:
+				print("YES")
+				c+=1
+				break
+		if c>=1:
+			break
